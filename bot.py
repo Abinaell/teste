19d7949d8fcb8f2b49780da966b18ae998efb646
Abinaell <104871889+Abinaell@users.noreply.github.com>
2024-08-09 17:53:07 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,44 @@
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
+from telegram.ext import Application, CommandHandler, ContextTypes
+import json
 
-TOKEN = '7227344806:AAGsi2PITxi6ab51w7WL79PsV9YLmNBWYAg'
+# Substitua 'YOUR_TOKEN' pelo token do seu bot
+TOKEN = 'SEU_TOKEN_AQUI'
+GAME_URL = '7227344806:AAGsi2PITxi6ab51w7WL79PsV9YLmNBWYAg'  # Substitua pelo URL fornecido pelo ngrok
 
-def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Hello! I am your bot.')
+# Função para carregar usuários do arquivo JSON
+def load_users():
+    try:
+        with open('users.json', 'r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return {}
 
-def main():
-    updater = Updater(TOKEN)
-    dispatcher = updater.dispatcher
+# Função para salvar usuários no arquivo JSON
+def save_users(users):
+    with open('users.json', 'w') as file:
+        json.dump(users, file, indent=4)
 
-    dispatcher.add_handler(CommandHandler('start', start))
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    users = load_users()
+    user_id = str(update.message.from_user.id)
+    user_name = update.message.from_user.username or update.message.from_user.first_name  # Garantir que temos um nome
 
-    updater.start_polling()
-    updater.idle()
+    if user_id not in users:
+        users[user_id] = {'id': user_id, 'name': user_name, 'tokens': 0}
+        save_users(users)
+    else:
+        users[user_id]['name'] = user_name  # Atualizar o nome se necessário
+
+    game_url = f"{GAME_URL}?user_id={user_id}&user_name={user_name}"
+    keyboard = [[InlineKeyboardButton("Lançar Jogo", web_app=WebAppInfo(url=game_url))]]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await update.message.reply_text('Clique no botão abaixo para jogar!', reply_markup=reply_markup)
+
+def main() -> None:
+    application = Application.builder().token(TOKEN).build()
+    application.add_handler(CommandHandler("start", start))
+    application.run_polling()
 
 if __name__ == '__main__':
     main()
