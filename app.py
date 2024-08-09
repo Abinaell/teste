a98bb530ef83276d3893f3d951fa04741b3664fd
Abinaell <104871889+Abinaell@users.noreply.github.com>
2024-08-09 18:28:28 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
-from flask import Flask, render_template
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+# Função para lidar com o comando /start
+def start(update: Update, context: CallbackContext):
+    keyboard = [[InlineKeyboardButton("Play", url="https://your-miniapp-url.com")]]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    update.message.reply_text('Clique no botão abaixo para jogar!', reply_markup=reply_markup)
+
+# Configuração do bot
+def main():
+    updater = Updater("7227344806:AAGsi2PITxi6ab51w7WL79PsV9YLmNBWYAg", use_context=True)
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("start", start))
+
+    updater.start_polling()
+    updater.idle()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
+    main()
+
+# Configuração do Flask
+@app.route('/')
+def index():
+    return 'Hello World!'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
